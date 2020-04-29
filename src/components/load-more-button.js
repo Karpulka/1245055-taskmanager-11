04b1770ae6ea9e3c20cd1b1c49a30d14b65ec46c
Alyona Karpova <karpova.dev@yandex.ru>
2020-04-29 21:07:40 +0300

    добавлен абстрактный класс, добавлено наследование классов компонентов от абстрактного класса

--- src/components/load-more-button.js
+++ src/components/load-more-button.js
@@ -1,27 +1,11 @@
-import {createElement} from "../util";
+import AbstractComponent from "./abstract-component";
 
 const createLoadMoreTemplate = () => {
   return `<button class="load-more" type="button">load more</button>`;
 };
 
-export default class LoadMoreButton {
-  constructor() {
-    this._element = null;
-  }
-
+export default class LoadMoreButton extends AbstractComponent {
   getTemplate() {
     return createLoadMoreTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
